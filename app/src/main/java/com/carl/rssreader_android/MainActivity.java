a3abd94bf2e3a8af20aa6db3109c24ec88bf2e5d
xzw <ag123456>
2021-03-31 18:12:06 +0800

    我也不记得改了什么

--- app/src/main/java/com/carl/rssreader_android/MainActivity.java
+++ app/src/main/java/com/carl/rssreader_android/MainActivity.java
@@ -1,13 +1,24 @@
 package com.carl.rssreader_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import github.com.st235.lib_expandablebottombar.ExpandableBottomBar;
+import github.com.st235.lib_expandablebottombar.ExpandableBottomBarMenuItem;
+
 public class MainActivity extends AppCompatActivity {
+    private ExpandableBottomBar ebb_bottom_navi_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    protected void initView(){
+        ebb_bottom_navi_bar = findViewById(R.id.ebb_bottom_navi_bar);
     }
 }
---
+++ app/src/main/java/widget/FloatingNavigationBar.java
@@ -0,0 +1,39 @@
+package widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+/**
+ * @author 创建人 ：xiezhiwei
+ * @version 1.0
+ * @package 包名 ：widget
+ * @createTime 创建时间 ：1/15/21
+ * @modifyBy 修改人 ：xiezhiwei
+ * @modifyTime 修改时间 ：1/15/21
+ * @modifyMemo 修改备注：
+ */
+public class FloatingNavigationBar extends LinearLayout {
+
+
+
+    public FloatingNavigationBar(Context context) {
+        super(context);
+    }
+
+    public FloatingNavigationBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FloatingNavigationBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public FloatingNavigationBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+}
